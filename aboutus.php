c2adfd9ae2395cda88b997e3e8a2bc5fb1aedd4f
majed12345 <47870633+majed12345@users.noreply.github.com>
2023-12-10 18:58:35 -0600

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Hotel M & M Online Reservation</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div  class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >Hotel M & M Online Reservation</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+		<li><a href = "index.php">Home</a></li> |
+		<li><a href = "aboutus.php">About us</a></li> |
+		<li><a href = "contactus.php">Contact us</a></li> |
+		<li><a href = "gallery.php">Gallery</a></li> |
+		<li><a href = "dineandlounge.php">Resturant</a></li> |			
+		<li><a href = "reservation.php">Reservation</a></li> |
+		<li><a href = "rulesandregulation.php">Policy</a></li>
+	</ul>
+	<div style = "margin-left:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<strong><h3>ABOUT US</h3></strong>
+				<p style = "position:relative; float:left; width:850px;">
+Welcome to M & M Hotels, where hospitality meets excellence. Nestled in prime locations, our boutique hotels offer a seamless blend of luxury and comfort. With a commitment to personalized service, we strive to create memorable experiences for every guest. Discover contemporary elegance, attentive staff, and a welcoming atmosphere that sets M & M Hotels apart. Whether for business or leisure, make your stay extraordinary with us.</p>
+				<img style = "float:right;" src = "images/about.jpg" width = "250px" height = "250px" />
+				<br style = "clear:both;" />
+				<br />
+				<br />
+				<hr style = "border:1px dotted #000;" />
+				<br />
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/1.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Standard</h4></center>
+					<center><label></label> <label style = "color:red;">$ 25.00</label></center>
+				</div>
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/2.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Extra Bed</h4></center>
+					<center><label style = "color:red;">$ 25.00</label></center>
+				</div>
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/3.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Superior</h4></center>
+					<center><label></label> <label style = "color:red;">$ 45.00 </label></center>
+				</div>
+				<br style = "clear:both;"/>
+				<br />
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/4.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Super Deluxe</h4></center>
+					<center><label></label> <label style = "color:red;">$ 55.00 </label></center>
+				</div>
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/5.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Jr. Suite</h4></center>
+					<center><label></label> <label style = "color:red;">$ 90.00 </label></center>
+				</div>
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/6.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Executive Suite</h4></center>
+					<center><label></label> <label style = "color:red;">$ 110.00 </label></center>
+				</div>
+				<br style = "clear:both;"/>
+				<br />
+				<br />
+				<strong><h3>Amenities and Services</h3></strong>
+				<ul>
+					<li><label>24 Hour room service</label></li>
+					<li><label>21" Flat screen TV with cable service</label></li>
+					<li><label>Hot & cold shower</label></li>
+					<li><label>Refrigerator and mini bar on demand in all suites and superior rooms</label></li>
+					<li><label>Coffee & tea set, bottled water, organic tolletries in suites and superior rooms</label></li>
+					<li><label>Hair dryer in suite rooms</label></li>
+					<li><label>Personal safety boxes in every room</label></li>
+					<li><label>Laundry & pressing</label></li>
+					<li><label>Free use Wifi</label></li>
+					<li><label>In room massage services</label></li>
+					<li><label>Personal Safe in Every Room</label></li>
+					<li><label>Mini Bar</label></li>
+					<li><label>7 Function & Meeting Rooms</label></li>
+					<li><label>Road Trip Airport Transfers & Special City Tour</label></li>
+					<li><label>Swimming Pool</label></li>
+					<li><label>Gift Shop</label></li>
+					<li><label>Business Center</label></li>
+					<li><label>Free Parking for Guest</label></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	<div style = "text-align:right; margin-right:10px;" class = "navbar navbar-default navbar-fixed-bottom">
+		<label>&copy; Copyright M & M 2023 </label>
+	</div>
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
